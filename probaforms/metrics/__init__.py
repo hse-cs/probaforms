f315c6312319994c96d9cc8c34149c42cd1c0759
Михаил Гущин <mihailh@n306667.staff.corp.local>
2023-03-28 16:01:15 +0300

    kde based kl and js

--- probaforms/metrics/__init__.py
+++ probaforms/metrics/__init__.py
@@ -1,8 +1,8 @@
 from .fd import frechet_distance
 from .mmd import maximum_mean_discrepancy
 from .ks1d import kolmogorov_smirnov_1d
-from .div1d import kullback_leibler_1d
-from .div1d import jensen_shannon_1d
+from .div1d import kullback_leibler_1d, kullback_leibler_1d_kde
+from .div1d import jensen_shannon_1d, jensen_shannon_1d_kde
 
 
 __all__ = [
@@ -10,5 +10,7 @@ __all__ = [
     'maximum_mean_discrepancy',
     'kolmogorov_smirnov_1d',
     'kullback_leibler_1d',
-    'jensen_shannon_1d'
+    'jensen_shannon_1d',
+    'kullback_leibler_1d_kde',
+    'jensen_shannon_1d_kde'
 ]
--- probaforms/metrics/div1d.py
+++ probaforms/metrics/div1d.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .ks1d import _bootstrap_metric
+from sklearn.neighbors import KernelDensity
 
 
 def compute_probs1d(data, bins=10):
@@ -36,6 +37,37 @@ def _kl1d(data1, data2, bins=10):
 
 def _js1d(data1, data2, bins=10):
     p, q = get_probs1d(data1, data2, bins)
+    eps = 10**-5 / bins
+    return js_divergence(p+eps, q+eps)
+
+
+def compute_probs1d_kde(data, grid):
+    kd = KernelDensity(bandwidth='silverman')
+    kd.fit(data.reshape(-1, 1))
+    p = np.exp(kd.score_samples(grid))
+    p /= p.sum()
+    return p
+
+
+def get_probs1d_kde(data1, data2, bins=101):
+
+    data12 = np.concatenate((data1, data2), axis=0)
+    grid = np.linspace(data12.min(), data12.max(), bins).reshape(-1, 1)
+
+    p = compute_probs1d_kde(data1, grid)
+    q = compute_probs1d_kde(data2, grid)
+
+    return p, q
+
+
+def _kl1d_kde(data1, data2, bins=101):
+    p, q = get_probs1d_kde(data1, data2, bins)
+    eps = 10**-5 / bins
+    return kl_divergence(p+eps, q+eps)
+
+
+def _js1d_kde(data1, data2, bins=101):
+    p, q = get_probs1d_kde(data1, data2, bins)
     eps = 10**-5 / bins
     return js_divergence(p+eps, q+eps)
 
@@ -94,3 +126,60 @@ def jensen_shannon_1d(X_real, X_fake, n_iters=100, bins=10):
     '''
 
     return _bootstrap_metric(_js1d, X_real, X_fake, n_iters, bins)
+
+
+
+def kullback_leibler_1d_kde(X_real, X_fake, n_iters=100, bins=101):
+    '''
+    Calculates the Kullback-Leibler divergence for real and fake samples.
+    The function calculates metric values for each input feature using 1D KDE,
+    and then averaged them.
+
+    Parameters:
+    -----------
+    X_real: numpy.ndarray of shape [n_samples, n_features]
+        Real sample.
+    X_fake: numpy.ndarray of shape [n_samples, n_features]
+        Generated sample.
+    n_iters: int
+        The number of bootstrap iterations. Default = 100.
+    bins: int
+        The number of bins used to compute probability densities. Default = 10.
+
+    Return:
+    -------
+    distance: float
+        The estimated metric value.
+    Std: fload
+        The standard deviation of the value.
+    '''
+
+    return _bootstrap_metric(_kl1d_kde, X_real, X_fake, n_iters, bins)
+
+
+def jensen_shannon_1d_kde(X_real, X_fake, n_iters=100, bins=101):
+    '''
+    Calculates the Jensen-Shannon divergence for real and fake samples.
+    The function calculates metric values for each input feature using 1D KDE,
+    and then averaged them.
+
+    Parameters:
+    -----------
+    X_real: numpy.ndarray of shape [n_samples, n_features]
+        Real sample.
+    X_fake: numpy.ndarray of shape [n_samples, n_features]
+        Generated sample.
+    n_iters: int
+        The number of bootstrap iterations. Default = 100.
+    bins: int
+        The number of bins used to compute probability densities. Default = 10.
+
+    Return:
+    -------
+    distance: float
+        The estimated metric value.
+    Std: fload
+        The standard deviation of the value.
+    '''
+
+    return _bootstrap_metric(_js1d_kde, X_real, X_fake, n_iters, bins)
