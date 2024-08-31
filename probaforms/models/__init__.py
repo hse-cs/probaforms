6706fe0663f9ab36e0b62d9cf149f1b1d7b8ccc1
Egor Zavialov <zes-amur@list.ru>
2024-08-31 14:28:32 +0900

    added conditional normal model, docs changed

--- probaforms/models/__init__.py
+++ probaforms/models/__init__.py
@@ -1,10 +1,12 @@
 from .realnvp import RealNVP
 from .cvae import CVAE
 from .wgan import ConditionalWGAN
+from .cnormal import ConditionalNormal
 
 
 __all__ = [
     'RealNVP',
     'CVAE',
-    'ConditionalWGAN'
+    'ConditionalWGAN',
+    'ConditionalNormal'
 ]
